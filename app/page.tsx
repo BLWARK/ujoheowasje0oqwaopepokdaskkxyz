995fd1151f1c35fc847108ea9fd721bf565e1645
Ronald <rozu0980@gmail.com>
2024-11-16 08:58:56 +0700

    change

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import Image from "next/image";
+import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Hero from "@/components/page-components/hero/Hero";
