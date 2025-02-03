279563e44bd66fb294caaab7c9ca388ce9de30c1
Ronald <rozu0980@gmail.com>
2025-02-03 13:39:50 +0700

    fixing error

--- app/main/pricing/page.jsx
+++ app/main/pricing/page.jsx
@@ -1,6 +1,5 @@
 "use client"
 import React, { useState, useEffect } from 'react';
-import { FaCheckCircle } from 'react-icons/fa';
 import { FaCheck } from "react-icons/fa6";
 import AOS from "aos";
 import "aos/dist/aos.css";
